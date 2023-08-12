456d10f389d7f22bfecf1ba5b7d0d140a8cea95c
Rashadul Shaon <codershaon@gmail.com>
2023-08-12 20:58:42 +0600

    Add checkout page detailed pricing

--- src/Form/CheckoutType.php
+++ src/Form/CheckoutType.php
@@ -20,7 +20,7 @@ class CheckoutType extends AbstractType
                 'placeholder' => 'ফোন নাম্বার',
                 'pattern' => '[0-9]{11}'
             ]])
-            ->add('shippingMethod', null, ['label' => false, 'attr' => ['placeholder' => 'শিপিং মেথড']])
+            ->add('shippingMethod', null, ['label' => false, 'attr' => ['placeholder' => 'শিপিং মেথড', 'class' => 'shippingMethod']])
             ->add('address', TextType::class, ['label' => false, 'attr' => ['placeholder' => 'লোকেশন']])
             ->add('note', TextareaType::class, ['label' => false, 'attr' => ['placeholder' => 'নোট লিখতে পারেন (প্রোডাক্টের সাইজ/কালার ইত্যাদি)'], 'required' => false])
         ;
